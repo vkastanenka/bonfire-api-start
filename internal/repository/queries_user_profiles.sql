575bee507dce6240fe6d87afd04cbf2ce6c366fa
Victoria Kastanenka <vkastanenka@gmail.com>
2026-06-16 15:02:39 -0400

    refactored user profiles

--- internal/repository/queries_user_profiles.sql
+++ internal/repository/queries_user_profiles.sql
@@ -1,19 +1,51 @@
--- user_profiles
--- name: CreateUserProfile :one
+-- name: UserProfileCreate :one
 INSERT INTO
     user_profiles (user_id, display_name)
 VALUES
-    ($ 1, $ 2) RETURNING user_id,
-    created_at,
-    display_name;
+    ($ 1, $ 2) RETURNING *;
 
--- name: GetUserProfile :one
+-- name: UserProfileGet :one
 SELECT
-    user_id,
-    created_at,
-    updated_at,
-    display_name
+    *
 FROM
     user_profiles
 WHERE
+    user_id = $ 1
+LIMIT
+    1;
+
+-- name: UserProfileGetByDisplayName :one
+SELECT
+    *
+FROM
+    user_profiles
+WHERE
+    lower(display_name) = lower($ 1)
+LIMIT
+    1;
+
+-- name: UserProfileUpdateDisplayName :exec
+UPDATE
+    user_profiles
+SET
+    display_name = $ 2,
+    updated_at = CURRENT_TIMESTAMP
+WHERE
     user_id = $ 1;
+
+-- name: UserProfileDelete :exec
+DELETE FROM
+    user_profiles
+WHERE
+    user_id = $ 1;
+
+-- name: UserProfileCheckDisplayNameAvailability :one
+SELECT
+    NOT EXISTS (
+        SELECT
+            1
+        FROM
+            user_profiles
+        WHERE
+            lower(display_name) = lower($ 1)
+    ) AS available;
