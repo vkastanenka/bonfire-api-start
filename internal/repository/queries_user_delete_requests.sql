e25f568719dd86f4050c57dfe25a58a7d0389a96
Victoria Kastanenka <vkastanenka@gmail.com>
2026-06-16 14:58:58 -0400

    updated user delete requests

---
+++ internal/repository/queries_user_delete_requests.sql
@@ -0,0 +1,31 @@
+-- name: UserDeleteRequestCreate :one
+INSERT INTO
+    user_delete_requests (user_id, scheduled_at)
+VALUES
+    ($ 1, $ 2) RETURNING *;
+
+-- name: UserDeleteRequestGet :one
+SELECT
+    *
+FROM
+    user_delete_requests
+WHERE
+    user_id = $ 1
+LIMIT
+    1;
+
+-- name: UserDeleteRequestListDue :many
+SELECT
+    user_id
+FROM
+    user_delete_requests
+WHERE
+    scheduled_at <= CURRENT_TIMESTAMP
+ORDER BY
+    scheduled_at ASC;
+
+-- name: UserDeleteRequestDelete :exec
+DELETE FROM
+    user_delete_requests
+WHERE
+    user_id = $ 1;
