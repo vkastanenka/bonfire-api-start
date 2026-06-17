68327094ef23a1720a11f6434da766ca39ebfb30
Victoria Kastanenka <vkastanenka@gmail.com>
2026-06-17 19:37:16 -0400

    updates service auth

--- internal/auth/service_auth.go
+++ internal/auth/service_auth.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"errors"
 	"net/netip"
-	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -18,8 +17,6 @@ import (
 
 // Register runs the business logic for creating a new user account.
 func (s *AuthService) Register(ctx context.Context, req RegisterRequest) error {
-	req.Email = strings.ToLower(strings.TrimSpace(req.Email))
-
 	// Check if credentials are available
 	availability, err := s.store.UserCheckAvailability(ctx, repository.UserCheckAvailabilityParams{
 		Email:    req.Email,
