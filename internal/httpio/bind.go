e41df9c7c8ee1bfe95e8ab54d4fd5ec6692799d7
Victoria Kastanenka <vkastanenka@gmail.com>
2026-06-19 16:55:18 -0400

    updated bind.go

--- internal/httpio/bind.go
+++ internal/httpio/bind.go
@@ -6,22 +6,23 @@ import (
 	"bonfire-api/internal/validator"
 )
 
-// BindJSON
 func BindJSON[T any](w http.ResponseWriter, r *http.Request, validator *validator.Validator) (T, error) {
 	var req T
 	if err := DecodeJSON(w, r, &req); err != nil {
 		return req, err
 	}
 
-	// If the struct implements our Sanitizable interface, run it
-	if s, ok := any(&req).(Sanitizable); ok {
-		s.Sanitize()
-	}
+	runLifecycleHooks(&req)
 
-	// Validate
 	if err := validator.ValidateStruct(&req); err != nil {
 		return req, err
 	}
 
 	return req, nil
+}
+
+func runLifecycleHooks(v any) {
+	if s, ok := v.(Sanitizable); ok {
+		s.Sanitize()
+	}
 }
