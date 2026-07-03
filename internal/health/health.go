db3af967895066ae1f6f9cff9f54be2d3f3c386a
Victoria Kastanenka <vkastanenka@gmail.com>
2026-07-03 19:46:55 -0400

    updated

--- internal/health/health.go
+++ internal/health/health.go
@@ -11,13 +11,11 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// Handler
 type Handler struct {
 	db    *pgxpool.Pool
 	redis *redis.Client
 }
 
-// NewHandler
 func NewHandler(db *pgxpool.Pool, redis *redis.Client) *Handler {
 	return &Handler{
 		db:    db,
@@ -25,18 +23,14 @@ func NewHandler(db *pgxpool.Pool, redis *redis.Client) *Handler {
 	}
 }
 
-// Check performs validation
 func (h *Handler) HealthCheck(w http.ResponseWriter, r *http.Request) error {
-	// 2 second max deadline
 	ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
 	defer cancel()
 
-	// Verify PostgreSQL Connectivity
 	if err := h.db.Ping(ctx); err != nil {
 		return apperr.NewInternal(err, "")
 	}
 
-	// Verify Redis Connectivity
 	if err := h.redis.Ping(ctx).Err(); err != nil {
 		return apperr.NewInternal(err, "")
 	}
