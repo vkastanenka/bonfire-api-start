023bf1a4f5efae6aa0719e0081b0c4b97a0505a4
Victoria Kastanenka <vkastanenka@gmail.com>
2026-08-04 16:13:46 -0400

    refactored users queries

--- db/queries/users.sql
+++ db/queries/users.sql
@@ -1,37 +1,3 @@
--- name: UserCreateAggregate :exec
-WITH new_user AS (
-INSERT INTO users(id, created_at, updated_at, verified_at, preferred_presence, email, username, password_hash)
-        VALUES (@user_id, @user_created_at, @user_updated_at, @verified_at, @preferred_presence, @email, @username, @password_hash))
-    INSERT INTO user_profiles(user_id, created_at, updated_at, display_name, avatar_url)
-        VALUES (@user_id, @profile_created_at, @profile_updated_at, @display_name, @avatar_url);
-
--- name: UserGet :one
-SELECT
-    *
-FROM
-    user_aggregates
-WHERE
-    id = $1
-LIMIT 1;
-
--- name: UserGetByEmail :one
-SELECT
-    *
-FROM
-    user_aggregates
-WHERE
-    email = $1
-LIMIT 1;
-
--- name: UserGetByUsername :one
-SELECT
-    *
-FROM
-    user_aggregates
-WHERE
-    username = $1
-LIMIT 1;
-
 -- name: UserCheckAvailability :one
 SELECT
     NOT EXISTS (
@@ -49,29 +15,177 @@ SELECT
         WHERE
             users.username = $2)::boolean AS username_available;
 
+-- name: UserCreateAggregate :exec
+WITH new_user AS (
+INSERT INTO users(id, created_at, updated_at, verified_at, disabled_at, delete_scheduled_at, preferred_presence_until, preferred_presence, email, username, password_hash, phone)
+        VALUES (@id::uuid, @created_at::timestamptz, @updated_at::timestamptz, sqlc.narg('verified_at')::timestamptz, sqlc.narg('disabled_at')::timestamptz, sqlc.narg('delete_scheduled_at')::timestamptz, sqlc.narg('preferred_presence_until')::timestamptz, sqlc.narg('preferred_presence')::smallint, @email::citext, @username::citext, @password_hash::text, sqlc.narg('phone')::text)
+    RETURNING
+        id
+), new_profile AS (
+INSERT INTO user_profiles(user_id, created_at, updated_at, display_name, bio, avatar_url, banner_color)
+        VALUES ((
+                SELECT
+                    id
+                FROM
+                    new_user),
+                @profile_created_at::timestamptz,
+                @profile_updated_at::timestamptz,
+                @display_name::citext,
+                sqlc.narg('bio')::text,
+                sqlc.narg('avatar_url')::text,
+                sqlc.narg('banner_color')::text)
+        RETURNING
+            user_id);
+
+-- name: UserGet :one
+SELECT
+    id,
+    email,
+    username,
+    password_hash,
+    phone,
+    preferred_presence,
+    preferred_presence_until,
+    verified_at,
+    disabled_at,
+    delete_scheduled_at,
+    created_at,
+    updated_at
+FROM
+    users
+WHERE
+    id = @id::uuid
+LIMIT 1;
+
+-- name: UserGetAggregate :one
+SELECT
+    id,
+    email,
+    username,
+    phone,
+    display_name,
+    bio,
+    avatar_url,
+    banner_color,
+    preferred_presence,
+    preferred_presence_until,
+    verified_at,
+    disabled_at,
+    delete_scheduled_at,
+    created_at,
+    updated_at
+FROM
+    user_aggregates
+WHERE
+    id = @id::uuid
+LIMIT 1;
+
+-- name: UserGetByEmail :one
+SELECT
+    id,
+    email,
+    username,
+    password_hash,
+    phone,
+    preferred_presence,
+    preferred_presence_until,
+    verified_at,
+    disabled_at,
+    delete_scheduled_at,
+    created_at,
+    updated_at
+FROM
+    users
+WHERE
+    email = @email::citext
+LIMIT 1;
+
+-- name: UserGetByPhone :one
+SELECT
+    id,
+    email,
+    username,
+    password_hash,
+    phone,
+    preferred_presence,
+    preferred_presence_until,
+    verified_at,
+    disabled_at,
+    delete_scheduled_at,
+    created_at,
+    updated_at
+FROM
+    users
+WHERE
+    phone = @phone::text
+LIMIT 1;
+
+-- name: UserGetByUsername :one
+SELECT
+    id,
+    email,
+    username,
+    password_hash,
+    phone,
+    preferred_presence,
+    preferred_presence_until,
+    verified_at,
+    disabled_at,
+    delete_scheduled_at,
+    created_at,
+    updated_at
+FROM
+    users
+WHERE
+    username = @username::citext
+LIMIT 1;
+
+-- name: UserProfileUpsert :one
+INSERT INTO user_profiles(user_id, created_at, updated_at, display_name, bio, avatar_url, banner_color)
+    VALUES (@user_id::uuid, @created_at::timestamptz, @updated_at::timestamptz, @display_name::citext, sqlc.narg('bio')::text, sqlc.narg('avatar_url')::text, sqlc.narg('banner_color')::text)
+ON CONFLICT (user_id)
+    DO UPDATE SET
+        display_name = EXCLUDED.display_name,
+        bio = EXCLUDED.bio,
+        avatar_url = EXCLUDED.avatar_url,
+        banner_color = EXCLUDED.banner_color,
+        updated_at = EXCLUDED.updated_at
+    RETURNING
+        user_id,
+        created_at,
+        updated_at,
+        display_name,
+        bio,
+        avatar_url,
+        banner_color;
+
 -- name: UserUpdate :one
 UPDATE
     users
 SET
-    email = @email,
-    username = @username,
-    password_hash = @password_hash,
-    preferred_presence = @preferred_presence,
-    verified_at = @verified_at,
-    updated_at = @updated_at
+    email = @email::citext,
+    username = @username::citext,
+    password_hash = @password_hash::text,
+    phone = sqlc.narg('phone')::text,
+    preferred_presence = sqlc.narg('preferred_presence')::smallint,
+    preferred_presence_until = sqlc.narg('preferred_presence_until')::timestamptz,
+    verified_at = sqlc.narg('verified_at')::timestamptz,
+    disabled_at = sqlc.narg('disabled_at')::timestamptz,
+    delete_scheduled_at = sqlc.narg('delete_scheduled_at')::timestamptz,
+    updated_at = @updated_at::timestamptz
 WHERE
-    id = @id
+    id = @id::uuid
 RETURNING
-    *;
-
--- name: UserProfileUpsert :one
-INSERT INTO user_profiles(user_id, created_at, updated_at, display_name, avatar_url)
-    VALUES (@user_id, @created_at, @updated_at, @display_name, @avatar_url)
-ON CONFLICT (user_id)
-    DO UPDATE SET
-        display_name = EXCLUDED.display_name,
-        avatar_url = EXCLUDED.avatar_url,
-        updated_at = EXCLUDED.updated_at
-    RETURNING
-        *;
+    id,
+    email,
+    username,
+    password_hash,
+    phone,
+    preferred_presence,
+    preferred_presence_until,
+    verified_at,
+    disabled_at,
+    delete_scheduled_at,
+    created_at,
+    updated_at;
 
