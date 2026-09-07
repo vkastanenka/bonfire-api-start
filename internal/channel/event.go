98243d8fe9cde1d41322d842421a6292835194a9
Victoria Kastanenka <vkastanenka@gmail.com>
2026-09-07 16:35:29 -0400

    updated channel events

--- internal/channel/event.go
+++ internal/channel/event.go
@@ -43,6 +43,11 @@ type EventChannelUpdatedPayload struct {
 	MemberIDs        []fields.ID `json:"member_ids"`
 }
 
+type EventChannelUpdatedClientPayload struct {
+	ExcludeSessionID fields.ID `json:"excludeSessionId"`
+	Channel          *Channel  `json:"channel"`
+}
+
 type EventChannelMemberAddedPayload struct{}
 type EventChannelMemberUpdatedPayload struct{}
 type EventChannelMemberRemovedPayload struct{}
@@ -66,6 +71,33 @@ func NewChannelCreatedEventHandler(gw outbox.Broadcaster) outbox.Handler {
 			[]fields.ID{p.ExcludeSessionID},
 			EventChannelCreated,
 			payload,
+		)
+	}
+}
+
+func NewChannelUpdatedEventHandler(gw outbox.Broadcaster) outbox.Handler {
+	return func(ctx context.Context, payload json.RawMessage) error {
+		p, err := fields.ParseRawJSON[EventChannelUpdatedPayload](payload)
+		if err != nil {
+			return err
+		}
+
+		clientPayload := EventChannelUpdatedClientPayload{
+			ExcludeSessionID: p.ExcludeSessionID,
+			Channel:          p.Channel,
+		}
+
+		clientPayloadBytes, err := json.Marshal(clientPayload)
+		if err != nil {
+			return err
+		}
+
+		return gw.BroadcastToUsers(
+			ctx,
+			p.MemberIDs,
+			[]fields.ID{p.ExcludeSessionID},
+			EventChannelUpdated,
+			clientPayloadBytes,
 		)
 	}
 }
