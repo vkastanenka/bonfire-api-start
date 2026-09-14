0718e8d4414f0d66015d870dd7c916815397f5b7
Victoria Kastanenka <vkastanenka@gmail.com>
2026-09-14 16:34:37 -0400

    making updates

--- internal/bootstrap/cache.go
+++ internal/bootstrap/cache.go
@@ -1,13 +1,20 @@
 package bootstrap
 
 import (
-	"bonfire-api/internal/fields"
 	"bonfire-api/internal/presence"
 	"context"
+
+	"github.com/google/uuid"
 )
 
 type PresenceCache interface {
-	GetPresence(ctx context.Context, userID fields.ID) (presence.Presence, error)
-	GetBatchPresence(ctx context.Context, userIDs []fields.ID) (map[fields.ID]presence.Presence, error)
-	SetPresence(ctx context.Context, userID fields.ID, p presence.Presence) error
+	GetBatchNodeUsers(ctx context.Context, userIDs []uuid.UUID) (map[uuid.UUID][]uuid.UUID, error)
+	GetBatchPresence(ctx context.Context, userIDs []uuid.UUID) (map[uuid.UUID]presence.Presence, error)
+	GetPresence(ctx context.Context, userID uuid.UUID) (presence.Presence, error)
+	GetSessionNode(ctx context.Context, userID uuid.UUID, sessionID uuid.UUID) (uuid.UUID, bool, error)
+	Heartbeat(ctx context.Context, nodeID uuid.UUID, userID uuid.UUID, sessionID uuid.UUID) error
+	RegisterNodeSession(ctx context.Context, nodeID uuid.UUID, userID uuid.UUID, sessionID uuid.UUID, p presence.Presence) (bool, presence.Presence, error)
+	RemoveBatchNodeUsers(ctx context.Context, nodeID uuid.UUID, userIDs []uuid.UUID) error
+	SetPresence(ctx context.Context, userID uuid.UUID, p presence.Presence) error
+	UnregisterNodeSession(ctx context.Context, nodeID uuid.UUID, userID uuid.UUID, sessionID uuid.UUID) (bool, error)
 }
